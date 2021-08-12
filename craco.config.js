588a5e47a72840a4ec4590923705dcd64eb0702d
Ryosuke Izumi <ryosuke.izumi62@gmail.com>
2021-08-12 22:28:46 +0900

    alias available

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const CracoAlias = require('craco-alias');
+
 module.exports = {
   style: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
   },
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: '.',
+        tsConfigPath: './tsconfig.paths.json',
+      },
+    },
+  ],
 };
